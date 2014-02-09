bd00fb42e166d836db6a0122f08085d16d3ae7da
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2014-02-09 13:43:45 +0100

    Adds more basic methods to NSObject. Refs #9.

--- objcbridge/NSObject.nim
+++ objcbridge/NSObject.nim
@@ -2,4 +2,8 @@ import objcbridge/core, strutils, macros
 
 import_objc_class(NSObject, """<Foundation/Foundation.h>"""):
   proc new*(): NSObject
+  proc alloc*(): NSObject
+  proc dealloc*()
+  proc retain*(self: NSObject): NSObject
   proc release*(self: NSObject)
+  proc autorelease*(self: NSObject): NSObject
